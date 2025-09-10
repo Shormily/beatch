c14eef1f7671c018173c95584a85f76c3d772f32
Ashik Ali Shanto <shanto902@gmail.com>
2025-09-10 14:40:34 +0600

    auth cors fix v2

--- src/api/auth/app/token.js
+++ src/api/auth/app/token.js
@@ -20,5 +20,5 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.status(r.status).send(text);
+  res.status(r.status || 200).send(text);
 }
