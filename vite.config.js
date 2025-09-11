4072933ca023c6246d031e45923e5f9e31929bb0
Shormily <shormilyrais@gmail.com>
2025-09-11 15:13:46 +0600

    vite-config-change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'   // Tailwind v4 হলে এটা ঠিক আছে
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
